b638c5db0a5c181bc5f5b5a30f8b3e9fbbddc56a
Marius Ungurean <umarius94@gmail.com>
2021-10-16 18:41:46 +0300

    CAN receive works, writing on display works.

--- EspEasy/APP_DATA/WRITE.ps1
+++ EspEasy/APP_DATA/WRITE.ps1
@@ -1,6 +1,6 @@
-$port=new-Object System.IO.Ports.SerialPort COM7,115200,None,8,one
+$port=new-Object System.IO.Ports.SerialPort COM6,115200,None,8,one
 $port.Open()
-$port.WriteLine("...")
+$port.WriteLine("Save")
   $message=$port.ReadLine()
   Write-Output $message
 $port.Close()
---
+++ TTGO_Display_Test/include/Cluster.h
@@ -0,0 +1,27 @@
+#ifndef _CLUSTER_H_INCLUDED
+#define _CLUSTER_H_INCLUDED
+
+
+#include <Arduino.h>
+
+// Define meter size
+#define M_SIZE 1
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+
+
+/**************Macro definitions*********/
+#define TFT_GREY 0x5AEB
+// #define TFT_ORANGE      0xFD20      /* 255, 165,   0 */
+
+
+/***************Public Variables*********/
+
+
+/***************Public Functions*********/
+uint8_t Cluster_Init(TFT_eSPI &passedTft);
+void Cluster_Main(void);
+void analogMeter(void);
+void plotNeedle(int value, byte ms_delay);
+
+#endif //multiple inclusion guard
